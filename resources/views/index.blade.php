9eac2229eb34f594df53f9084d21d2a11460b674
Bin Nguyễn <nguyenybin2015@gmail.com>
2023-02-12 15:37:43 +0700

    navbar update

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -10,45 +10,45 @@
 </head>
 <body>
     <header>
-        <nav class="navbar navbar-expand-sm navbar-light fixed-top" >
-              
-                <a class="navbar-brand" href="#">Binad</a>
-                <button class="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavId" aria-controls="collapsibleNavId"
-                    aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="collapsibleNavId">
-                    <ul class="navbar-nav me-auto mt-2 mt-lg-0">
-                        <li class="nav-item">
-                            <a class="nav-link active" href="#" aria-current="page">Home <span class="visually-hidden">(current)</span></a>
-                        </li>
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" id="dropdownId" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Services</a>
-                            <div class="dropdown-menu" aria-labelledby="dropdownId">
-                                <a class="dropdown-item" href="#">Đăng kí</a>
-                                <a class="dropdown-item" href="#">Tên miền miễn phí và trả phí</a>
-                                <a class="dropdown-item" href="#">Bảng giá tên miền</a>
-                                <a class="dropdown-item" href="#">WHOIS</a>
-                            </div>
-                        </li>
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" id="dropdownId" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Giới thiệu về Binad</a>
-                            <div class="dropdown-menu" aria-labelledby="dropdownId">
-                                <a href="#" class="drop-item">Công ty</a>
-                                <a href="#" class="drop-item">Điều khoản và điều kiện</a>
-                                <a href="#" class="drop-item">Liên hệ với chúng tôi</a>
-                            </div>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Support</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Đăng nhập</a>
-                        </li>
-                    </ul>
-                </div>
-          
-        </nav>
+        <nav class="navbar navbar-expand-lg navbar-dark p-3">
+            <div class="container-fluid">
+              <a class="navbar-brand" href="#">Binad</a>
+              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+             
+              <div class=" collapse navbar-collapse" id="navbarNavDropdown">
+                <ul class="navbar-nav ms-auto ">
+                    <li class="nav-item">
+                        <a class="nav-link active" href="#" aria-current="page">Home <span class="visually-hidden">(current)</span></a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="dropdownId" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Services</a>
+                        <div class="dropdown-menu" aria-labelledby="dropdownId">
+                            <a class="dropdown-item" href="#">Đăng kí</a>
+                            <a class="dropdown-item" href="#">Tên miền miễn phí và trả phí</a>
+                            <a class="dropdown-item" href="#">Bảng giá tên miền</a>
+                            <a class="dropdown-item" href="#">WHOIS</a>
+                        </div>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="dropdownId" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Giới thiệu về Binad</a>
+                        <div class="dropdown-menu" aria-labelledby="dropdownId">
+                            <a class="dropdown-item" href="#">Công ty</a>
+                            <a class="dropdown-item" href="#">Điều khoản và điều kiện</a>
+                            <a class="dropdown-item" href="#">Liên hệ với chúng tôi</a>
+                        </div>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Support</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="login">Đăng nhập</a>
+                    </li>
+                </ul>
+              </div>
+            </div>
+            </nav>
     </header>
     <div class="container">
 
