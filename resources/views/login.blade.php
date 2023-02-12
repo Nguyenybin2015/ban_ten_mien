dc28f91beae94d2cb0847d63bf3ca05e1db7f652
Bin Nguyễn <nguyenybin2015@gmail.com>
2023-02-12 15:25:46 +0700

    new changes

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -39,7 +39,7 @@
           <div class="card-body p-5 shadow-5 text-center">
             <h2 class="fw-bold mb-5">Sign up now</h2>
             <form>
-              <!-- 2 column grid layout with text inputs for the first and last names -->
+              
               <div class="row">
                 <div class="col-md-6 mb-4">
                   <div class="form-outline">
