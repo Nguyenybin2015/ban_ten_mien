cdffe7a376bbdb4ba2ec5dc04042a7148bd22dd2
Bin Nguyễn <nguyenybin2015@gmail.com>
2023-02-16 23:20:33 +0700

    reset login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,17 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    {{-- <link rel="stylesheet" href="{{ asset('css/style.css') }}"> --}}
-    {{-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous"> --}}
-
-    <title>Login</title>
-</head>
-<body>
-  
-    <x-guest-layout>
+<x-guest-layout>
         
         <x-jet-authentication-card>
             <x-slot name="logo">
@@ -59,6 +46,4 @@
                 </div>
             </form>
         </x-jet-authentication-card>
-    </x-guest-layout>    
-</body>
-</html>
+    </x-guest-layout>  
